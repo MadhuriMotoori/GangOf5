466b997ec404a97ae479bdcba173ead33d97d0d0
kjayashankar <kjayashankar@yahoo.com>
2016-04-16 21:29:05 -0700

    Added Mapper class which deals with image rotations and map layout details

---
+++ Greenfoot/Mapper.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mapper here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mapper extends Actor
+{
+    /**
+     * Act - do whatever the Mapper wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+   // public void act() 
+   // {
+        // Add your action code here.
+   // }
+    public Mapper(){
+    }
+    
+    public void setImageRotation(int angle,String imageString)
+    {
+        GreenfootImage image = new GreenfootImage(imageString);
+        image.rotate(angle);
+        setImage(image);
+    }
+    
+    public Mapper(int map)
+    {
+        if(map==2) setImage("Wall1.png");
+        if(map==3) setImageRotation(180,"Wall1.png");
+        if(map==4) setImage("WallEdge.png");
+        if(map==5) setImageRotation(90,"WallEdge.png");
+        if(map==6) setImageRotation(90,"Wall1.png");
+        if(map==7) setImageRotation(270,"Wall1.png");
+        if(map==8) setImage("SWallEdge.png");
+        if(map==9) setImageRotation(90,"SWallEdge.png");
+        if(map==10) setImageRotation(180,"SWallEdge.png");
+        if(map==11) setImageRotation(270,"SWallEdge.png");
+        if(map==12) setImage("Corner.png");
+        if(map==13) {
+            setImage("Corner.png");
+            setImageRotation(90,"Corner.png");
+        }
+        if(map==14) setImageRotation(180,"Corner.png");
+        if(map==15) setImageRotation(270,"Corner.png");
+        if(map==18) setImage("RCorner.png");
+        if(map==19) setImageRotation(180,"RCorner.png");
+        if(map==20) setImageRotation(90,"RCorner.png");
+        if(map==21) setImageRotation(270,"RCorner.png");
+        if(map==22) setImageRotation(0,"Gate.png");
+    }
+}
