8007fb3c798e1bee901d0295b70245a8c6128698
kjayashankar <kjayashankar@yahoo.com>
2016-04-20 09:41:12 -0700

    Adding pacman movement class initial commit

---
+++ Greenfoot/PacMovement.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PacMovement here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PacMovement extends Actor
+{
+    public PacMovement(Pacman pacman){
+        
+    }
+    /**
+     * Act - do whatever the PacMovement wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    
+    public void setPacImage(){
+    }
+    
+    public void update(){
+    
+    }
+}
