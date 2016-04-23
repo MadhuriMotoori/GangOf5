a5962e167c4bc7d54d2e698c4e36908bd8b30b2b
sreeram767 <sreeram.muddu@gmail.com>
2016-04-23 14:32:50 -0700

    Adding ghost classes required to implement Factory Pattern

---
+++ Greenfoot/Ghost5.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ghost5 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ghost5 extends Ghost
+{
+    /**
+     * Act - do whatever the Ghost5 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
