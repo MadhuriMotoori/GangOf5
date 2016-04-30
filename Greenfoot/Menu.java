c5c34cb380beca35d6ff88f380cd580e19f2f742
Madhuri Motoori <madhukmr.555@gmail.com>
2016-04-30 01:14:39 -0700

    adding code for gameover screen

--- Greenfoot/Menu.java
+++ Greenfoot/Menu.java
@@ -1,19 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Calendar;
 
-/**
- * Write a description of class Menu here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Menu extends Actor
 {
-    /**
-     * Act - do whatever the Menu wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+    public Menu() 
     {
-        // Add your action code here.
-    }    
+    }
+    public Menu(int ID)
+    {
+        switch(ID) {
+            case 7: makeImage("Game Over ", "   Your score is: ");
+            break;
+        }
+    }
+    private void makeImage(String title, String prefix)
+    {
+        GreenfootImage img = new GreenfootImage(730, 700);
+        img.setColor(new Color(0,0,0, 128));
+        img.fillRect(0, 0, 730, 700);
+        Font ft = img.getFont();
+        ft = ft.deriveFont(72.0f);
+        img.setFont(ft);
+        img.setColor(Color.WHITE);
+        img.drawString(title, 200, 200);
+        img.drawString(prefix, 100, 300);
+        setImage(img);
+    }
 }
